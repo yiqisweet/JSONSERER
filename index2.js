1340fb3da1adc151ac6517a9c9b886a8186dd67a
yiqisweet <mynameislxmax@outlook.com>
2017-10-31 18:39:58 +0800

    master 上的 bug 已经被紧急修复

---
+++ index2.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: randy
+ * @Date:   2017-10-31 06:39:26
+ * @Email:  mynameislxmax@outlook.com
+ * @Last modified by:   randy
+ * @Last modified time: 2017-10-31 06:39:35
+ */
+console.log('成功修复了 bug');
