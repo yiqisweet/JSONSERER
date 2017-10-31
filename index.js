1ed4d9af2a97750973aeb15a9fcf161ff4d959a0
yiqisweet <mynameislxmax@outlook.com>
2017-10-31 18:18:59 +0800

    fixbug

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @Author: randy
+ * @Date:   2017-10-31 05:45:49
+ * @Email:  mynameislxmax@outlook.com
+ * @Last modified by:   randy
+ * @Last modified time: 2017-10-31 06:17:57
+ */
+1212222
+22222
+222
+222
+2222
+222222222
+sss
+ssssspwd
