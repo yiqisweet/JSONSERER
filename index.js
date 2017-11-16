bd78aeb4c7783e2a116ff834eace7069e95fe635
Randy Lee <mynameislxmax@outlook.com>
2017-11-16 12:33:37 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: randy
- * @Date:   2017-10-31 05:45:49
- * @Email:  mynameislxmax@outlook.com
- * @Last modified by:   randy
- * @Last modified time: 2017-10-31 06:34:00
- */
-console.log('我正在测试分支写项目');
-console.log('正在写代码')
